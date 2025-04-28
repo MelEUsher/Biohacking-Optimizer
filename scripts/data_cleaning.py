29c4f3bddba9b5dabef1e71899ecf81f7701a8d7
Melissa Usher <MelEUsher17@gmail.com>
2025-04-28 17:16:58 -0500

    format data_cleaning.py to test installation of black formatter

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -2,7 +2,6 @@ import pandas as pd
 
 
 def drop_missing_rows(df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Drops rows with any missing values from the DataFrame.
-    """
+    """Drops rows with any missing values from the DataFrame."""
+
     return df.dropna()
